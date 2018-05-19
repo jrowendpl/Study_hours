ae86d6b2edf6979ec88803c586190005abf23dac
jrowendpl <jrowen@dontpaniclabs.com>
2018-05-19 14:25:36 -0500

    Initial commit

---
+++ AGN Study Hours/AGN Study Hours/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AGN_Study_Hours.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
